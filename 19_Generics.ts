54660bc52d6649cc384d020baeabadfa916a626c
Pradyumna <45138354+Pradhyumna789@users.noreply.github.com>
2023-12-18 04:27:38 +0530

    One more - Last change

--- 19_Generics.ts
+++ 19_Generics.ts
@@ -1,1 +1,12 @@
 // Refer to this -> https://github.com/mike-north/typescript-courses/blob/main/packages/notes-ts-fundamentals-v4/src/13-generics.ts
+
+// Example of two generics
+function makeTuple<T, U>(arg1: T, arg2: U): [T, U] {
+  return [arg1, arg2]
+}
+
+const zz = makeTuple(1, 2)
+const zzz = makeTuple('foo', window)
+
+
+
